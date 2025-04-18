e558d7d1f478c798110c3baa7c83f5b49f8202ff
PSSPL-Mobile <61695158+PSSPL-Mobile@users.noreply.github.com>
2025-04-18 15:05:08 +0530

    Initial commit

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const geminiApiKey = 'AIzaSyDFmMv_orvfaCZIx2BJ26aS0Bnq5-2WhNY'; // Use .env in prod
+  static const geminiApiKey = 'YOUR_GEMINI_API_KEY_HERE-2WhNY';
   static const geminiUrl =
       'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
 }
